fc53b55dfa05c3cc505def1917aaadbe9829db72
InfiniteMalice <115899879+InfiniteMalice@users.noreply.github.com>
2026-01-05 13:35:34 -0500

    Prepare package metadata for PyPI

--- src/gepa_dapo_grn/__init__.py
+++ src/gepa_dapo_grn/__init__.py
@@ -1,7 +1,5 @@
 """GEPA-aware DAPO training library with optional GRN support."""
 
-from importlib.metadata import PackageNotFoundError, version
-
 from gepa_dapo_grn.config import DAPOConfig, GRNConfig, RewardMixerConfig
 from gepa_dapo_grn.curriculum import CurriculumTracker
 from gepa_dapo_grn.dapo_core import DAPOTrainer
@@ -9,19 +7,16 @@ from gepa_dapo_grn.gepa_interfaces import GEPAFeedback
 from gepa_dapo_grn.grn import GlobalResponseNorm
 from gepa_dapo_grn.safety_controller import SafetyController
 
-try:
-    __version__ = version("gepa-dapo-grn")
-except PackageNotFoundError:
-    __version__ = "0.0.0"
+__version__ = "0.1.0"
 
 __all__ = [
-    "__version__",
     "DAPOConfig",
-    "DAPOTrainer",
-    "CurriculumTracker",
-    "GEPAFeedback",
-    "GlobalResponseNorm",
     "GRNConfig",
     "RewardMixerConfig",
+    "GEPAFeedback",
+    "DAPOTrainer",
+    "CurriculumTracker",
     "SafetyController",
+    "GlobalResponseNorm",
+    "__version__",
 ]
